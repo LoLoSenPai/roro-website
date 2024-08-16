4031d22adac645b9e86917ca4750383b8f46a789
LoLoSenPai <loic.dlugosz@gmail.com>
2024-08-16 17:44:07 +0200

    chore: Update check-eligibility API route to return Response objects

--- src/app/api/check-eligibility/route.js
+++ src/app/api/check-eligibility/route.js
@@ -3,11 +3,13 @@ import { authOptions } from '../auth/[...nextauth]/options';
 import connectToDatabase from '@/lib/mongodb';
 import Claim from '@/models/Claim';
 
-export default async function handler(req, res) {
+export async function GET(req) {
     const session = await getServerSession({ req, ...authOptions });
 
     if (!session) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return new Response(JSON.stringify({ error: 'Unauthorized' }), {
+            status: 401,
+        });
     }
 
     const twitterHandle = session.user.handle;
@@ -15,15 +17,34 @@ export default async function handler(req, res) {
     try {
         await connectToDatabase();
 
+        // Vérifier si l'utilisateur a déjà claim
         const claim = await Claim.findOne({ twitterHandle });
 
         if (claim && claim.claimed) {
-            return res.status(200).json({ eligible: false });
+            return new Response(
+                JSON.stringify({ eligible: false, message: "Already claimed." }),
+                { status: 200 }
+            );
+        }
+
+        // Récupérer l'éligibilité de la base de données
+        const distribution = await Claim.findOne({ twitterHandle });
+
+        if (distribution) {
+            return new Response(
+                JSON.stringify({ eligible: true, tokens: distribution.tokens }),
+                { status: 200 }
+            );
         } else {
-            return res.status(200).json({ eligible: true, tokens: 1000 });
+            return new Response(
+                JSON.stringify({ eligible: false, message: "Not eligible for this phase." }),
+                { status: 200 }
+            );
         }
     } catch (error) {
-        console.error('Failed to check eligibility:', error);
-        res.status(500).json({ error: 'Failed to check eligibility' });
+        console.error('Error fetching eligibility:', error);
+        return new Response(JSON.stringify({ error: 'Failed to fetch eligibility' }), {
+            status: 500,
+        });
     }
 }
